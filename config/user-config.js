b95b4581d00eb3deb950ae94be507050cef706a1
Efabien <fabien@topteam.tech>
2020-10-23 15:55:24 +0300

    use dotenv

--- config/user-config.js
+++ config/user-config.js
@@ -1,5 +1,8 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
 module.exports = Object.freeze({
   HOST: 'https://noaservices.atlassian.net',
   username: 'fabien@topteam.tech',
-  TOKEN: process.env.JIRA_TOKEN || 'Khdq18R5JVN6V25cD3UPFD31'
+  TOKEN: process.env.JIRA_TOKEN
 });
