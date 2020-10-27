54fff1ce914d7dcee4f2f508692bc7d3e90189db
Efabien <fabien@topteam.tech>
2020-10-27 12:45:24 +0300

    correct path to config file

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 const yaml = require('js-yaml');
 const fs   = require('fs');
+const path = require('path');
 
-const config = yaml.safeLoad(fs.readFileSync('./cli-ify.yml', 'utf8'));
+const config = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, '../cli-ify.yml'), 'utf8'));
 module.exports = config;
